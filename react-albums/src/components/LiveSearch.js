e804cf4f68c060c395315c76aa097d32bf6328e1
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-02-07 08:43:45 -0800

    useVisualModeTest is installed

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,11 +1,21 @@
-import React, { Fragment, useState } from "react";
-
+import React, { Fragment, useState, useEffect } from "react";
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
+const axios = require('axios');
+
 
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+
+useEffect(()=>{
+  axios.get(`https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`).then((response) => {
+    console.log(response.data.results);
+    setResults(response.data.results);
+  },[term]);
+  
+});
+  
 
   return (
     <Fragment>
